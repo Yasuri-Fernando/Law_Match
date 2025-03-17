c90c02b3264f7f911ad876cb1b80dbf7064dec01
DinuliJ <dinurajapaksha1216@gmail.com>
2025-03-18 03:07:25 +0530

    Update App.jsx
    - Switched import to AdminContext from AppContext
    - Added React Router routes for admin dashboard, appointments, lawyer list, and add lawyer pages
    - Integrated ToastContainer for notifications
    - Fixed redundant Navbar and Login component calls

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,22 +1,34 @@
-import React, { useContext } from 'react'
-import Login from './pages/Login'
+import React, {useContext} from 'react'
+import Login from './pages/Lawyer/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 //import { AppContext } from './context/AppContext';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllApointments from './pages/Admin/AllApointments';
+import AddLawyer from './pages/Admin/AddLawyer';
+import LawyersList from './pages/Admin/LawyersList';
 
 const App = () => {
 
   const {aToken} = useContext(AdminContext)
-  //localStorage.setItem('aToken', 'dummy_token');
-  
   return aToken ? (
-    <div className='bg-[#F8F9FD]' >
-      
+    <div className = 'bg-[#F8F9FD]' >
       <ToastContainer/>
-      <Navbar />
-      {!aToken && <Login />}
+      <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>} />
+          <Route path='/all-appointments' element={<AllApointments/>} />
+          <Route path='/add-lawyer' element={<AddLawyer/>} />
+          <Route path='/lawyer-list' element={<LawyersList/>} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <> 
