d7f44ea98f7d68272b216a04f73c87e2bc0894a9
DinuliJ <dinurajapaksha1216@gmail.com>
2025-03-20 02:45:42 +0530

    Designing Add Lawyer page &  Adding menu items to sidebar with icons and text
    -Create select tag with options for experience and fee input field
    -Creating input fields and sections for lawyer appointment booking system
    -Adding CSS properties to various elements in the web layout.
    -Styling input fields using CSS properties

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,25 +7,33 @@ const Sidebar = () => {
 
     const {aToken} = useContext(AdminContext)
   return (
-    <div>
+    <div className='min-h-screen bg-white border-r'>
         {
-            aToken && <ul>
-             <NavLink to={'/admin-dashboard'}>
+            aToken && <ul className='text-[#515151] mt-5'>
+             <NavLink  className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-lg ${isActive
+                                    ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                    : 'text-[#515151] hover:bg-gray-50'}`}to={'/admin-dashboard'}>
                 <img src={assets.home_icon} alt=''/>
-                <p>Dashboard</p>
+                <p className="text-[15px]">Dashboard</p>
             </NavLink>   
 
-            <NavLink to={'/all-appointments'}>
+            <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-lg ${isActive
+                                    ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                    : ''}`} to={'/all-appointments'}>
                 <img src={assets.appointment_icon} alt=''/>
                 <p>Appointments</p>
             </NavLink>
 
-            <NavLink to={'/add-lawyer'}>
+            <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-lg ${isActive
+                                    ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                    : ''}`} to={'/add-lawyer'}>
                 <img src={assets.add_icon} alt=''/>
                 <p>Add Lawyer</p>
             </NavLink>
 
-            <NavLink to={'/lawyer-list'}>
+            <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer rounded-lg ${isActive
+                                    ? 'bg-[#F2F3FF] border-r-4 border-primary'
+                                    : ''}`} to={'/lawyer-list'}>
                 <img src={assets.people_icon} alt=''/>
                 <p>Lawyers List</p>
             </NavLink>
