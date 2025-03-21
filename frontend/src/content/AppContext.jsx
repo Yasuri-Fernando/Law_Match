69b55879a2a271b4385d7640bc8b3b23c0bf04d0
DinuliJ <dinurajapaksha1216@gmail.com>
2025-03-22 05:00:16 +0530

    Setting up environment variables in the front end

--- frontend/src/content/AppContext.jsx
+++ frontend/src/content/AppContext.jsx
@@ -1,14 +1,36 @@
-import { createContext } from "react";
-import {lawyers} from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const currencySymbol = '$'
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const[lawyers,setLawyers] = useState([])
     
     const value = {
         lawyers,
         currencySymbol
     }
+
+    const getlawyersData = async () => {
+        try{
+
+            const {data} = await axios.get(backendUrl  + '/api/lawyer/list')
+            if (data.success) {
+                setLawyers(data.lawyers)
+
+            }
+
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() =>{
+        getlawyersData()
+    },[])
+
     return(
         <AppContext.Provider value={value}>
             {props.children}
