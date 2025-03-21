8ad636301776ed11d1c4e015712f645c3dcc4b09
chathumikethmini <kethminichathumi@gmail.com>
2025-03-22 01:00:24 +0530

    Edited NGO s in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ function Home() {
                 </svg>
                 Find Courts
               </Link>
-              <Link to="/ngos" className="bg-gray-600 text-black px-8 py-3 rounded-lg font-semibold hover:bg-gray-700 flex items-center">
+              <Link to="/ngos" className="bg-white text-black px-8 py-3 rounded-lg font-semibold hover:bg-white flex items-center">
                 <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                 </svg>
