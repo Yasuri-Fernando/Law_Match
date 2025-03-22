c6584f4f861b2ed9136f3e9de9151cb30d61f831
DinuliJ <dinurajapaksha1216@gmail.com>
2025-03-23 03:07:43 +0530

    Implementing multiple middleware functions for user authentication and data update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { registerUser,loginUser } from '../controllers/userController.js';
+import { registerUser,loginUser, getProfile, updateProfile } from '../controllers/userController.js';
+import authUser from '../middlewares/authUser.js';
+import upload from '../middlewares/multer.js';
+
 
 const userRouter = express.Router()  // Creating a new router
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)  // Defining POST /register route
+
+userRouter.get('/get-profile',authUser,getProfile)
+userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
 
 export default userRouter;  // Exporting the router
